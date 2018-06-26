3fbb8f439bbd27a2ea5450153544c3cc85934c71
ibezkorovayniy <i.bezkorovayniy@gmail.com>
2018-06-26 14:01:28 +0300

    Refactoring

--- src/main/java/solution/XmlAnalyzer.java
+++ src/main/java/solution/XmlAnalyzer.java
@@ -1,5 +1,7 @@
 package solution;
 
+
+import org.apache.log4j.BasicConfigurator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
@@ -10,24 +12,29 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class XmlAnalyzer {
 
     private static final String CHARSET_NAME = "utf8";
-    private static final String DEFAULT_ID = "make-everything-ok-button";
-    private static final String CSS_QUERY = ".btn";
-    private static final Logger LOGGER = LoggerFactory.getLogger(XmlAnalyzer.class);
+    private static final String DEFAULT_BUTTON_ID = "make-everything-ok-button";
+    private static final String ALL_BUTTONS_QUERY = ".btn";
+    private static Logger LOGGER = LoggerFactory.getLogger(XmlAnalyzer.class);
 
     public static void main(String[] args) {
 
+        BasicConfigurator.configure();
         String originalHtmlPath = args[0];
         String diffCaseHtmlPath = args[1];
         String originalElementId;
 
         if (args.length == 2) {
-            originalElementId = DEFAULT_ID;
+            originalElementId = DEFAULT_BUTTON_ID;
         } else {
             originalElementId = args[2];
         }
@@ -35,65 +42,32 @@ public class XmlAnalyzer {
         // Getting original element from the html file
         Optional<Element> buttonOpt = findElementById(new File(originalHtmlPath), originalElementId);
 
-        if (!buttonOpt.isPresent()) {
-            throw new RuntimeException("No such element");
-        }
-        Element originalElement = buttonOpt.get();
-
-        List<String> originalElementAttributes = originalElement
-                .attributes()
-                .asList()
-                .stream()
-                .map(attr -> attr.getKey() + " = " + attr.getValue())
-                .collect(Collectors.toList());
+        Optional<String> stringifiedAttributesOpt = buttonOpt.map(button ->
+                button.attributes().asList().stream()
+                        .map(attr -> attr.getKey() + " = " + attr.getValue())
+                        .collect(Collectors.joining(", "))
+        );
 
         // Logging all element attributes
-        System.out.println("Original element attributes: ");
-        originalElementAttributes.forEach(System.out::println);
+        stringifiedAttributesOpt.ifPresent(attrs -> LOGGER.info("Original element attributes: [{}]", attrs));
 
         // Creating set of strings with values of attributes
         Set<String> originalAttributes = new HashSet<>();
-
         buttonOpt.ifPresent(button -> originalAttributes
                 .addAll(button.attributes().asList().stream()
                         .map(Attribute::getValue)
                         .collect(Collectors.toSet())));
 
-        Optional<Elements> elementsOpt = findElementsByQuery(new File(diffCaseHtmlPath), CSS_QUERY);
+        Optional<Elements> elementsOpt = findElementsByQuery(new File(diffCaseHtmlPath), ALL_BUTTONS_QUERY);
 
         if (!elementsOpt.isPresent()) {
             throw new RuntimeException("No such elements");
         }
         Elements targetElements = elementsOpt.get();
 
-        List<ElementWrapper> elementWrappers = new ArrayList<>();
+        ElementWrapper targetElement = getTargetElement(targetElements, originalAttributes);
 
-        targetElements.forEach(element -> elementWrappers
-                .add(new ElementWrapper(element, element.attributes()
-                        .asList()
-                        .stream()
-                        .map(Attribute::getValue)
-                        .collect(Collectors.toSet()))));
-
-        int max = 0;
-        ElementWrapper targetElement = null;
-
-        for (ElementWrapper elementWrapper : elementWrappers) {
-            Set<String> intersection = new HashSet<>(elementWrapper.getAttributeSet());
-            intersection.retainAll(originalAttributes);
-            if (max < intersection.size()) {
-                max = intersection.size();
-                targetElement = elementWrapper;
-            }
-        }
-
-        System.out.println("----------------------------------------");
-
-        if (targetElement != null) {
-            System.out.println("Target element attributes :" + targetElement.getAttributeSet());
-            System.out.println("\nPath to target element: " + "html > body > div.wrapper > div."
-                    + targetElement.getElement().cssSelector());
-        }
+        pathBuilder(targetElement);
 
     }
 
@@ -127,10 +101,40 @@ public class XmlAnalyzer {
         }
     }
 
+    private static ElementWrapper getTargetElement(Elements elements, Set<String> originalAttributes) {
+        List<ElementWrapper> elementWrappers = new ArrayList<>();
+        elements.forEach(element -> elementWrappers
+                .add(new ElementWrapper(element, element.attributes()
+                        .asList()
+                        .stream()
+                        .map(Attribute::getValue)
+                        .collect(Collectors.toSet()))));
+        int max = 0;
+        ElementWrapper targetElement = null;
+        for (ElementWrapper elementWrapper : elementWrappers) {
+            Set<String> intersection = new HashSet<>(elementWrapper.getAttributeSet());
+            intersection.retainAll(originalAttributes);
+            if (max < intersection.size()) {
+                max = intersection.size();
+                targetElement = elementWrapper;
+            }
+        }
+        return targetElement;
+    }
+
+    private static void pathBuilder(ElementWrapper elementWrapper) {
+        System.out.println("----------------------------------------");
+
+        if (elementWrapper != null) {
+            System.out.println("Target element attributes: " + elementWrapper.getAttributeSet());
+            System.out.println("\nPath to target element: " + "html > body > div.wrapper > div."
+                    + elementWrapper.getElement().cssSelector());
+        }
+    }
+
     static class ElementWrapper {
 
         private Element element;
-
         private Set<String> attributeSet;
 
         public ElementWrapper(Element element, Set<String> attributeSet) {
@@ -155,5 +159,3 @@ public class XmlAnalyzer {
         }
     }
 }
-
-
